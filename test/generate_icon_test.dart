0cdd6646ac11ec248a080c79b034b611d601c747
Sufiya Ansari <sufiyaahmed95@gmail.com>
2026-09-12 00:12:26 +0530

    updated the apps launcher icon

--- test/generate_icon_test.dart
+++ test/generate_icon_test.dart
@@ -3,15 +3,23 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Generate App Icon', (WidgetTester tester) async {
+    tester.view.physicalSize = const Size(512, 512);
+    tester.view.devicePixelRatio = 1.0;
+    addTearDown(() {
+      tester.view.resetPhysicalSize();
+      tester.view.resetDevicePixelRatio();
+    });
+
     await tester.pumpWidget(
-      MaterialApp(
+      const MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
           backgroundColor: Colors.white,
           body: Center(
             child: Icon(
               Icons.local_florist,
               color: Color(0xFFFF6B9E),
-              size: 512,
+              size: 320,
             ),
           ),
         ),
